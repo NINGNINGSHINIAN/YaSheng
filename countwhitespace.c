7a95126dfa03f0b8d978bce4f6c8da53f61fa41f
YaSheng <1465263578@qq.com>
2021-05-02 01:46:08 +0800

    The C Programming Language

---
+++ countwhitespace.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+void main()
+{
+	int c, i, nwhite, nother, ndigit[10];
+	
+	nwhite = nother = 0;
+	for(i = 0;i < 10;i++)
+		ndigit[i] = 0;
+	while ((c = getchar()) != EOF){
+		switch(c){
+			case '0':case '1':case '2':case '3':case '4':
+			case '5':case '6':case '7':case '8':case '9':
+				ndigit[c-'0']++;
+				break;
+			case ' ':
+			case '\n':
+			case '\t':
+			nwhite++;
+			default:
+			nother++;
+			break;	
+		}
+	}
+	printf("digits =");
+	for(i = 0;i < 10;i++)
+		printf("%d", ndigit[i]);
+	printf(",white space = %d, other = %d\n",
+	nwhite,nother);
+}
+
+/*void main()
+{
+	int c;
+	
+	while ((c = getchar()) != EOF){
+		putchar(c);
+		c = getchar();
+	}
+}
+
+#include <stdlib.h>
+int main()
+{
+	printf("%d\n", EOF);
+	system("pause");
+	return 0;
+}*/ 
